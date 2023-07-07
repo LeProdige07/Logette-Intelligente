2860b9dbc914bc32bb3ed270fe1192ea8fb25353
Don Makila <makiladon14@gmail.com>
2023-07-07 12:37:48 +0100

    secure forms

--- logette/routes/web.php
+++ logette/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,5 @@ Route::group(['middleware' => ['auth']], function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+URL::forceScheme('https');
